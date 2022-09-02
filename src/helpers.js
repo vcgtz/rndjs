52d1370e9d3cd2ec008e7f899b3527bb6ef6ef28
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-09-02 16:09:48 -0500

    Format using prettier

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,19 @@
 /**
  * Reference: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/from#generador_de_secuencia_(rango)
- * 
+ *
  */
-const range = (start, end, step = 1) => (
-  Array.from({ length: (end - start) / step + 1 }, (_, i) => start + (i * step))
-);
+const range = (start, end, step = 1) =>
+  Array.from({ length: (end - start) / step + 1 }, (_, i) => start + i * step);
 
-const getLowerCharsArray = () => range('a'.charCodeAt(0), 'z'.charCodeAt(0))
-  .map(c => String.fromCharCode(c));
+const getLowerCharsArray = () =>
+  range('a'.charCodeAt(0), 'z'.charCodeAt(0)).map((c) =>
+    String.fromCharCode(c)
+  );
 
-const getUpperCharsArray = () => range('A'.charCodeAt(0), 'Z'.charCodeAt(0))
-  .map(c => String.fromCharCode(c));
+const getUpperCharsArray = () =>
+  range('A'.charCodeAt(0), 'Z'.charCodeAt(0)).map((c) =>
+    String.fromCharCode(c)
+  );
 
 module.exports = {
   range,
